fc57722253fca4a957004afc27bd51e18174e995
Radek Rocławski <r.roclawski@ankara.pl>
2024-01-23 16:02:11 +0100

    feat: basic bot - human chat bubbles organized

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,8 +1,34 @@
+import { useState } from 'react';
 import './Chatbot.css';
 
 const ChatBot = () => {
+    const [isChatbotVisible, setChatbotVisible] = useState(true);
+
+    const handleClick = () => {
+        setChatbotVisible(!isChatbotVisible);
+    };
+
     return (
-        <div className="main-window">Chatbot</div>
-    )
+        <>
+            {isChatbotVisible ? (
+                <div className="chat-bot" onClick={handleClick}>Chatbot</div>
+            ) : (
+                <div className="chat-bot main-window">
+                    <div>
+                        <button onClick={handleClick}>X</button>
+                    </div>
+                    <div className='bot-container'>
+                        <div className='bot-message'>Bot:</div>
+                        <div className="bot-message">Hello! How can I help you?</div>
+                    </div>
+                    <div className='user-container'>
+                        <div className='user-message'>User:</div>
+                        <div className="user-message">I am a new client.</div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
+
 export default ChatBot;
